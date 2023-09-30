a9aa44f70c09fe3efda5392d8f7b54e1e8b267c8
Gabriel lebel <2239650@cegeplimoilou.ca>
2023-09-30 11:00:33 -0400

    Test de commit

--- src/main/java/a23/sim203/tp2/CalculatriceApp.java
+++ src/main/java/a23/sim203/tp2/CalculatriceApp.java
@@ -1,8 +1,14 @@
 package a23.sim203.tp2;
 
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class CalculatriceApp {
+public class CalculatriceApp extends Application {
 
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
 }
