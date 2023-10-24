032a91592e2cb3026e577c819b7ceba43074b426
mirona2240773 <2240773@cegeplimoilou.ca>
2023-10-24 00:31:44 -0400

    MoteurCalcul au complet

--- src/main/java/a23/sim203/tp2/modele/MoteurCalcul.java
+++ src/main/java/a23/sim203/tp2/modele/MoteurCalcul.java
@@ -8,36 +8,34 @@ import java.util.*;
 public class MoteurCalcul {
 
     // ajoutez les attributs pour stocker les équations et les variables
-    Set<Equation> equationsHashSet;
-    Map<String, Double> variablesHashMap;
-    Map<Equation, Set<String>> referencesMap;
-    Set<Constant> variablesSet;
+    Map<String, Constant> variablesHashMap;
+    Map<String, Equation> equationMap;
 
     Equation equation;
-
 
 
     public MoteurCalcul() {
         License.iConfirmNonCommercialUse("Cegep Limoilou");
 
-        equationsHashSet = new HashSet<>();
         variablesHashMap = new HashMap<>();
-        referencesMap = new HashMap<>();
-        variablesSet = new HashSet<>();
+        equationMap = new HashMap<>();
     }
 
 
     private Set<String> determineToutesVariablesRequises() {
-
-        return null;
+        Set<String> stringSet = new HashSet<>();
+        for (Equation equation : getToutesLesEquations()) {
+            stringSet.addAll(equation.getElementsRequis());
+        }
+        return stringSet;
     }
 
     private void ajouteVariable(String variable, double valeur) {
-        variablesSet.add(new Constant(variable, valeur));
+        variablesHashMap.put(variable, new Constant(variable, valeur));
     }
 
     public void setValeurVariable(String nomVariable, double valeur) {
-        variablesHashMap.replace(nomVariable, valeur);
+        variablesHashMap.replace(nomVariable, new Constant(nomVariable, valeur));
     }
 
 
@@ -45,10 +43,19 @@ public class MoteurCalcul {
         int indexEquals = nouvelleEquation.indexOf('=');
         String nomEquation = nouvelleEquation.substring(0, indexEquals);
         String expression = nouvelleEquation.substring(indexEquals + 1);
+
         try {
+
             equation = new Equation(nomEquation, expression);
-            equationsHashSet.add(equation);
-            referencesMap.put(equation, new HashSet<>());
+
+            if (isExpressionValid(equation)) {
+                variablesHashMap.remove(nomEquation); //enlever variable du meme nom si elle existe
+                equationMap.put(nomEquation, equation);
+                for (String element : equation.getElementsRequis()) {
+                    addVariableIfNotExistant(element);
+                }
+                circularDependencyCheck(nomEquation);
+            }
 
         } catch (RuntimeException e) {
             if (Objects.equals(e.getMessage(), "Mauvais nom")) {
@@ -59,47 +66,158 @@ public class MoteurCalcul {
         }
     }
 
-
-    public void effaceEquation(String nomEquation) {
-        equationsHashSet.removeIf((equation) -> equation.getNom().equals(nomEquation));
+    private void addVariableIfNotExistant(String nomVariable) {
+        if (!getToutesLesVariables().contains(nomVariable)) {
+            variablesHashMap.put(nomVariable, new Constant(nomVariable, Double.NaN));
+        }
     }
 
-    public double calcule(String nomEquation) {
+    private void circularDependencyCheck(String nomEquation) {
+        if (hasCircularDependency(nomEquation)) {
+            effaceEquation(nomEquation);
+            showAlertEquationNonValide();
+        }
+    }
 
-        return 0; // à changer
+    private boolean isExpressionValid(Equation equation) {
+        return equation.getElementsRequis().size() != 0;
+    }
+
+
+    public void effaceEquation(String nomEquation) {
+        Equation equationToBeDeleted = getEquationFromString(nomEquation);
+        if (isEquationReferredTo(nomEquation)) {
+            ajouteVariable(nomEquation, calcule(nomEquation));
+        }
+
+        Set<String> references = equationToBeDeleted.getElementsRequis();
+
+        equationMap.remove(equationToBeDeleted.getNom());
+
+        //shave off variables non utilisées
+        for (String element : references) {
+            if (isVariable(element) && !isElementReferredTo(element, null)) {
+                variablesHashMap.remove(element);
+            }
+        }
+    }
+
+    /**
+     * Itère dans la Map de références pour vérifier si la variable est utilisée par une équation autre que celle indiquée
+     *
+     * @param element       variable dont on vérifie les références
+     * @param ignoredString équation dont on ignore les références
+     */
+    private boolean isElementReferredTo(String element, String ignoredString) {
+        Iterator<String> iterator = equationMap.keySet().iterator();
+        String currentEquation;
+
+        while (iterator.hasNext()) {
+            currentEquation = iterator.next();
+            if (!currentEquation.equals(ignoredString)) {
+                if (equationMap.get(currentEquation).getElementsRequis().contains(element))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean isEquationReferredTo(String nomEquation) {
+        return isElementReferredTo(nomEquation, nomEquation);
+    }
+
+//    private void replaceEquationByVariable(Equation equation) {
+//        Iterator<Equation> iterator = referencesMap.keySet().iterator();
+//        Equation currentEquation;
+//        ajouteVariable(equation.getNom(), calcule(equation));
+//        boolean equationWasReplaced = false;
+//
+//        while (iterator.hasNext()) {
+//            currentEquation = iterator.next();
+//            if (currentEquation != equation) {
+//                if (currentEquation.getElementsRequis().remove(equation.getNom())) {
+//                    currentEquation.getElementsRequis().add()
+//                    equationWasReplaced = true;
+//                }
+//            }
+//        }
+//    }
+
+    public double calcule(String nomEquation) {
+        return calcule(getEquationFromString(nomEquation)); // à changer
     }
 
     public double calcule(Equation equation) {
+        Set<String> elementsRequis = equation.getElementsRequis();
+        Constant[] tableauConstant = new Constant[elementsRequis.size()];
+        int compteur = 0;
 
-        return 0; // à changer
+        for (String element : elementsRequis) {
+            if (isEquation(element)) {
+                tableauConstant[compteur++] = new Constant(element, calcule(element));
+            } else {
+                tableauConstant[compteur++] = variablesHashMap.get(element);
+            }
+        }
+        Expression expression = new Expression(equation.getExpression(), tableauConstant);
+        return expression.calculate();
 
     }
 
     public Collection<String> getToutesLesVariables() {
-
-
-        return null; // à changer
+        return variablesHashMap.keySet();
     }
 
-    public Equation getToutesLesEquations() {
+    public Set<Equation> getToutesLesEquations() {
+        Set<Equation> equationSet = new HashSet<>();
+        for (String s :
+                equationMap.keySet()) {
+            equationSet.add(equationMap.get(s));
+        }
+        return equationSet;
+    }
 
-        return null; // à changer
+    private boolean hasCircularDependency(String nomEquation) {
+        return hasCircularDependencyInternal(new HashSet<>(), nomEquation);
+    }
 
+    private boolean hasCircularDependencyInternal(HashSet<String> parentReferences, String currentElement) {
+        if (parentReferences.contains(currentElement)) {
+            return true;
+        }
+
+        if (isEquation(currentElement)) {
+            parentReferences.add(currentElement);
+            Set<String> dependencies = getEquationFromString(currentElement).getElementsRequis();
+
+            for (String dependency : dependencies) {
+                if (isEquation(dependency) && hasCircularDependencyInternal((HashSet<String>) parentReferences.clone(), dependency)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean isEquation(String nomEquation) {
+        return equationMap.containsKey(nomEquation);
+    }
+
+    private boolean isVariable(String nomVariable) {
+        return variablesHashMap.containsKey(nomVariable);
     }
 
 
+    public Map<String, Constant> getVariableValueMap() {
 
-
-
-    public Map<Object, Object> getVariableValueMap() {
-
-        return null; // à changer
+        return variablesHashMap; // à changer
 
     }
 
-    public Map<Object, Object> getEquationMap() {
+    public Map<String, Equation> getEquationMap() {
 
-        return null; // à changer
+        return equationMap; // à changer
 
     }
 
@@ -115,20 +233,23 @@ public class MoteurCalcul {
 
         License.iConfirmNonCommercialUse("Cegep Limoilou");
 
-        Constant A0 = new Constant("a0",3);
-        Constant B0 = new Constant("b0",3);
+        Constant A0 = new Constant("a0", 3);
+        Constant B0 = new Constant("b0", 3);
 
 
         Expression e1 = new Expression("3+4+a0+b0", A0, B0);
-        Expression e2 = new Expression("3+4+a0+b0", new Constant[]{A0,B0});// alternative avec tableau
+        Expression e2 = new Expression("3+4+a0+b0", new Constant[]{A0, B0});// alternative avec tableau
 
-        System.out.println("e1="+e1.calculate());
-        System.out.println("e2="+e2.calculate());
+        System.out.println("e1=" + e1.calculate());
+        System.out.println("e2=" + e2.calculate());
 
     }
 
+    private Equation getEquationFromString(String nomEquation) {
+        return equationMap.get(nomEquation);
+    }
+
     /**
-     *
      * @return l'équation
      */
     public Equation getEquation() {
